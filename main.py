49cbefbc4ad439377eff0118837b6c1ac9838e89
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-15 20:55:30 -0300

    aulapython

---
+++ main.py
@@ -0,0 +1,19 @@
+#formula de baskara
+a = 4
+b = -20
+c = 24
+
+delta = b**2 - 4*a*c
+
+x1 = (-b + delta**(1/2))/(2*a)
+x2 = (-b - delta**(1/2))/(2*a)
+
+print ('As raízes são', x1, x2)
+
+#aumento de 15%
+
+salario = 4000
+aumento = 4000*15/100
+print('aumento', aumento)
+print('novo salário', salario+aumento)
+
